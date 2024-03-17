fa4011ae722aa6e1947553ef0d3a8b2367db7855
Tigran <t-oganesyan00@yandex.ru>
2024-03-18 00:05:32 +0300

    front: изменение sidebar на нижнюю закрепленную панель навигации

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {FEED_PAGE, MAIN_PAGE} from "./utils/routes/path";
 import React from "react";
 import Header from "./components/Header";
 import Main from "./pages/Main";
-import Sidebar from "./components/Sidebar";
+import Navigation from "./components/Navigation";
 import Feed from "./pages/Feed";
 
 const App = () => {
@@ -13,15 +13,13 @@ const App = () => {
               <Header />
               <section className="content">
                   <div className="container">
-                      <div className="row">
-                          <Sidebar/>
-                          <Routes>
-                              <Route path={MAIN_PAGE} element={<Main/>} exact/>
-                              <Route path={FEED_PAGE} element={<Feed/>} exact/>
-                          </Routes>
-                      </div>
+                      <Routes>
+                          <Route path={MAIN_PAGE} element={<Main/>} exact/>
+                          <Route path={FEED_PAGE} element={<Feed/>} exact/>
+                      </Routes>
                   </div>
               </section>
+              <Navigation/>
           </BrowserRouter>
       </>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import {FEED_PAGE, FRIEND_PAGE, MESSAGE_PAGE, PROFILE_PAGE} from "../utils/routes/path";
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="navigation__element">
+                            <NavLink to={PROFILE_PAGE} className="navigation__link">
+                                <i className="fa fa-user" aria-hidden="true"></i><span>Профиль</span>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="navigation__element">
+                            <NavLink to={FEED_PAGE} className="navigation__link">
+                                <i className="fa fa-newspaper-o" aria-hidden="true"></i><span>Лента</span>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="navigation__element">
+                            <NavLink to={FRIEND_PAGE} className="navigation__link">
+                                <i className='fa fa-users' aria-hidden="true"></i><span>Друзья</span>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="navigation__element">
+                            <NavLink to={MESSAGE_PAGE} className="navigation__link">
+                                <i className="fa fa-envelope" aria-hidden="true"></i><span>Сообщения</span>
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
